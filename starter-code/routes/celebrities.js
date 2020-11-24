df5c96f5b5cc5f0bd3e2b329e2053a694936477d
Dirk Kelderman <dirkkelderman@gmail.com>
2020-11-24 19:39:42 +0100

    Iteration 4

--- starter-code/routes/celebrities.js
+++ starter-code/routes/celebrities.js
@@ -2,17 +2,32 @@ const express = require('express');
 const router  = express.Router();
 const Celebrity = require('../models/celebrity');
 
+/// this is our get Route
 router.get('/', (req, res, next) => {
     Celebrity.find()
         .then(celebrities => {
             res.render('celebrities/index', {celebrities});
         })
         .catch(err => {
-            console.log(`Error getting celebrities from DB: ${err}`)
             next(err);
         });
 });
 
+router.get('/new', (req, res, next) => {
+    res.render('celebrities/new');
+});
+
+router.post('/new', (req, res, next) => {
+    const {name, occupation,catchPhrase} = req.body   
+    Celebrity.create({name, occupation,catchPhrase})
+    .then(() => {
+        res.redirect('/celebrities');
+    })
+    .catch(err => {
+        console.log(`Error adding data to DB: ${err}`)
+        next(err);
+    });
+});
 
 router.get('/:id', (req, res, next) => {
     Celebrity.findById(req.params.id)
@@ -24,6 +39,9 @@ router.get('/:id', (req, res, next) => {
             next(err);
         });
 });
+
+
+
 
 
 
